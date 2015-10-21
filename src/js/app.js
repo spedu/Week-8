dfefa1215918348ec7ae24e2b21c99a0d8b108e5
JaZahn <jcleveng@fas.harvard.edu>
2015-10-21 17:07:35 -0400

    reverting to a non-solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,8 @@
 var app = angular.module('logintrest', []);
 
-app.controller('FormController', function(){
-  var vm = this;
-  vm.logins = [];
-
-  vm.login = function(){
-    console.log("logging in!");
-    vm.logins.push({username: vm.username, time:new Date()});
-    vm.username = '';
-    vm.password = '';
+app.controller('FormController', ['$scope', function($scope){
+  $scope.submitForm = function(){
+    console.log($scope.loginForm);
+    
   };
-
-});
+}]);
--- src/tests/specs.js
+++ src/tests/specs.js
@@ -1,24 +1,10 @@
 describe('FormController', function(){
-  var formController;
-  beforeEach(angular.mock.module('logintrest'));
-  beforeEach(angular.mock.inject(function($controller){
-    formController = $controller('FormController');
-  }));
+
   it('should clear the values of username and password', function(){
-    formController.username = "something";
-    formController.password = "somethingelse";
-    formController.login();
-    expect(formController.username).toBe('');
-    expect(formController.password).toBe('');
+
   });
 
   it('should add the username and time to the logins array', function(){
-    formController.username = "something";
-    formController.password = "somethingelse";
-    expect(formController.logins.length).toBe(0);
-    formController.login();
-    expect(formController.logins.length).toBe(1);
-    expect(formController.logins[0].username).toBe("something");
 
   });
 
